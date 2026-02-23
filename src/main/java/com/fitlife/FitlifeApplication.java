b24c1395f344d7e48369807c2116cec8352a027d
Huy Le <huy30987@gmail.com>
2026-02-23 08:40:34 +0700

    chore: update FitlifeApplication main class

--- src/main/java/com/fitlife/FitlifeApplication.java
+++ src/main/java/com/fitlife/FitlifeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FitlifeEcosystemApplication {
+public class FitlifeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FitlifeEcosystemApplication.class, args);
+        SpringApplication.run(FitlifeApplication.class, args);
     }
 
 }
